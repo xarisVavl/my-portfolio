c643d4f8fd98343d08636591533a1946112ece5c
Charis Vavliaras <xarisvavliaras@icloud.com>
2024-07-11 10:34:53 +0300

    added disabled property on visit site

--- src/components/ObjectList.js
+++ src/components/ObjectList.js
@@ -9,19 +9,23 @@ const Object = () => {
     {
       id:1,
       name:"Ticketing system",
-      description:"I designed and developed a fault management system for my diploma project using PHP, Bootstrap CSS, and jQuery. The system utilizes AJAX for data fetching from the server and was built and tested using XAMPP.",
+      description:"I designed and developed a fault management system for my diploma project using PHP, Bootstrap CSS, and jQuery. The system utilizes AJAX for data fetching from the server and was built and tested using XAMPP. Unfortunately, the site is not accessible because GitHub Pages does not support hosting PHP projects.",
       img:ticketinSystemImage,
       linkSite:"",
-      linkCode:"https://github.com/xarisVavl/ticketing_system"
+      linkCode:"https://github.com/xarisVavl/ticketing_system",
+      button: "disabled"
       
     },
     {
       id:2,
       name:"E-shop",
-      description:"A simple e-shop that fetches data from Fake store api.It is build in react and bootstrap css. This project  was a good chance for me to learn about  props, hooks and the philosophy of Reactavi programming.",
+      description:"A simple e-shop that fetches data from Fake store api.It is build in react and bootstrap css. This project  was a good chance for me to learn about  props, hooks and the philosophy of Reactive programming.",
       img:storeImage,
       linkSite:"https://xarisvavl.github.io/store-react-main/",
-      linkCode:"https://github.com/xarisVavl/store-react-main"
+      linkCode:"https://github.com/xarisVavl/store-react-main",
+      button: ""
+
+      
       
     },
     {
@@ -30,29 +34,23 @@ const Object = () => {
       description:"Building a basic chess game using pure JavaScript was an enlightening experience in programming. Although it currently is not  optimized for playing, this project served as an excellent introduction to JavaScript programming and object oriented programming.",
       img:chessImage,
       linkSite:"https://xarisvavl.github.io/chess_game/",
-      linkCode:"https://github.com/xarisVavl/chess_game"
+      linkCode:"https://github.com/xarisVavl/chess_game",
+      button: ""
       
     }
     ,
     {
       id:4,
       name:"Old portfolio",
-      description:"This was my first approach of creating a portfolio. I used pure css and javascript.",
+      description:"This was my first attempt at creating a portfolio, using only pure CSS and JavaScript. I have since replaced it with what you see now.",
       img:oldPortfolioImage,
       linkSite:"https://xarisvavl.github.io/mypersonalportfolio/",
-      linkCode:"https://github.com/xarisVavl/mypersonalportfolio"
+      linkCode:"https://github.com/xarisVavl/mypersonalportfolio",
+      button: ""
       
     }
-    ,
-    {
-      id:4,
-      name:"Old portfolio",
-      description:"This was my first approach of creating a portfolio. I used pure css and javascript.",
-      img:oldPortfolioImage,
-      linkSite:"https://xarisvavl.github.io/mypersonalportfolio/",
-      linkCode:"https://github.com/xarisVavl/mypersonalportfolio"
-      
-    }
+    
+
 
 
   ]
@@ -64,14 +62,14 @@ const Object = () => {
      <div className="card " key={object.id}>
      <div className="image overflow-hidden bg-white rounded-top">
        <img src={object.img} className="card-img-top object-fit-contain" alt=""/> 
-       <h4 className="image-title d-flex align-items-center justify-content-center flex-column "><img className="eye"src ={eyeImage} alt =""></img></h4>
+       <h4 className="image-effect d-flex align-items-center justify-content-center flex-column "><img className="eye"src ={eyeImage} alt =""></img></h4>
      </div>
         
-     <div className="card-body">
+     <div className="card-body d-flex flex-column ">
        <h5 className="card-title">{object.name}</h5>
-       <p className="card-text">{object.description}</p>
+       <p className="card-text flex-fill">{object.description}</p>
        <div className="d-flex justify-content-center">
-         <a href={object.linkSite} className="btn btn-warning m-2" target="blank">Visit site</a>
+         <a href={object.linkSite} className={`btn btn-warning m-2 ${object.button}`} target="blank" disabled>Visit site</a>
          <a href={object.linkCode} className="btn btn-secondary m-2" target="blank">Visit code</a>
        </div>
      </div>
